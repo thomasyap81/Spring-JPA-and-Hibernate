92f4f85f33210c81b147b8e034735fc149711800
Thomas Yap <yaptathong@gmail.com>
2024-04-17 22:19:57 +0800

    using Spring Jdbc to insert and delete not in hard code

--- learn-jpa-and-hibernate/src/main/java/com/thomas/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/thomas/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.thomas.springboot.learnjpaandhibernate.course.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 	
@@ -12,7 +14,11 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert();
+		repository.insert(new Course(1, "learn AWS now!", "in28minutes"));
+		repository.insert(new Course(2, "learn Azure now!", "in28minutes"));
+		repository.insert(new Course(3, "learn DevOps now!", "in28minutes"));
+		
+		repository.deleteById(1);
 		
 	}
 	
