b98d707651593d35158413e584fb4a23f7b9a3eb
Thomas Yap <yaptathong@gmail.com>
2024-04-18 21:40:19 +0800

    more feature on Spring Data Jpa: to create own method

--- learn-jpa-and-hibernate/src/main/java/com/thomas/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/thomas/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -32,6 +32,15 @@ public class CourseCommandLineRunner implements CommandLineRunner{
 		System.out.println(repository.findById(2l));
 		System.out.println(repository.findById(3l));
 		
+		System.out.println(repository.findAll());
+		System.out.println(repository.count());
+		
+		System.out.println(repository.findByAuthor("in28minutes"));
+		System.out.println(repository.findByAuthor(""));
+		
+		System.out.println(repository.findByName("learn Azure jpa!"));
+		System.out.println(repository.findByName("learn AWS jpa!"));
+		
 	}
 	
 	
